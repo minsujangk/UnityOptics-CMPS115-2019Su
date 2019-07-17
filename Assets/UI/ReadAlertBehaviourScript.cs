6e979f8c95a25b8652705b564f5ea8def5e5952e
Matthew Rhea <m.b.rhea31@gmail.com>
2019-07-17 13:00:02 -0700

    Optics data retrieval (#8)
    * Fix for occluded objects being 'isVisible == true' in Renderer.
    https://trello.com/c/gW84uZIW
    
    * Minor fix for latest commit
    
    * Making objects in-game movable (by Jason)
    https://trello.com/c/WH0sD1x5
    
    * 'Read Advertisement' function implementation
    https://trello.com/c/HTVxIZ2U
    
    * Pickup: Throwing object by 'E' keypress (by Jason)
    
    * Load advertisements from JSON file
    https://trello.com/c/RCAUX2AX

---
+++ Assets/UI/ReadAlertBehaviourScript.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ReadAlertBehaviourScript : MonoBehaviour
+{
+    GameObject[] adObjects;
+    public GameObject readAlert;
+
+    Sprite activeReadingAdSprite;
+
+    public GameObject AdDescription;
+    public GameObject AdImageView;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        adObjects = GameObject.FindGameObjectsWithTag("AdObject");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool isActive = false;
+        var camPos = Camera.main.transform.position;
+        float activeReadingAdDistance = 10.0F;
+
+        foreach (var adObject in adObjects){
+            AdBehaviorScript sc = adObject.GetComponent<AdBehaviorScript>();
+            bool isDrawReadAlert = sc.isDrawReadAlert;
+
+            //Debug.Log(adObject.name + " in read is " + isDrawReadAlert);
+
+            if (isDrawReadAlert)
+            {
+                if (activeReadingAdDistance > sc.adDistance)
+                {
+                    activeReadingAdSprite = sc.AdSprite;
+                    activeReadingAdDistance = sc.adDistance;
+                }
+            }
+
+            isActive |= isDrawReadAlert;
+
+        }
+
+        readAlert.SetActive(isActive);
+        if (!isActive)
+        {
+            activeReadingAdSprite = null;
+            activeReadingAdDistance = 10.0F;
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            toggleActiveAd();
+        }
+    }
+
+    void toggleActiveAd()
+    {
+        if (!AdDescription.activeSelf && activeReadingAdSprite != null)
+        {
+            AdImageView.GetComponent<Image>().sprite = activeReadingAdSprite;
+        }
+        AdDescription.SetActive(!AdDescription.activeSelf);
+    }
+}
