56424c0bc1789dd031512633865d8ee30ba8bb8f
mrhea <m.b.rhea31@gmail.com>
2019-07-17 16:42:51 -0700

    Update link for MacEditorInstaller for Unity.pkg

--- TravisScripts/install.sh
+++ TravisScripts/install.sh
@@ -2,12 +2,11 @@
 
 # Installs Unity3D in Travis container
 
-echo 'Downloading from https://beta.unity3d.com/download/8ea4afdbfa47/UnityDownloadAssistant.dmg: '
-curl -o Unity.pkg https://beta.unity3d.com/download/8ea4afdbfa47/UnityDownloadAssistant.dmg
+echo 'Downloading from https://beta.unity3d.com/download/8ea4afdbfa47/MacEditorInstaller/Unity.pkg: '
+curl -o Unity.pkg https://beta.unity3d.com/download/8ea4afdbfa47/MacEditorInstaller/Unity.pkg
 if [ $? -ne 0 ]; then { echo "Download failed"; exit $?; } fi
 
 
 echo 'Installing Unity.pkg'
 sudo installer -dumplog -package Unity.pkg -target /
-
 
